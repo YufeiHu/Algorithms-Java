7284e6bda8c8863a50f1e17eef3caede392d4623
Yufei Hu <yufeihu@g.ucla.edu>
2020-04-29 20:27:29 -0700

    Create UnionFind.java

---
+++ UnionFind.java
@@ -0,0 +1,49 @@
+class UnionFind {
+    public int[] parents;
+    public int[] sizeComponent;
+    public int numElements;
+    public int numComponents;
+    
+    public UnionFind(int numElements) {
+        this.parents = IntStream.range(0, numElements).toArray();
+        this.sizeComponent = new int[numElements];
+        Arrays.fill(this.sizeComponent, 1);
+        this.numElements = numElements;
+        this.numComponents = numElements;
+    }
+    
+    public int find(int idx) {
+        // find its root first
+        int root = idx;
+        while (root != this.parents[root]) {
+            root = this.parents[root];
+        }
+        
+        // path compression
+        while (idx != root) {
+            int idxNext = this.parents[idx];
+            this.parents[idx] = root;
+            idx = idxNext;
+        }
+        
+        // return the root
+        return root;
+    }
+    
+    public void union(int i1, int i2) {
+        int root1 = find(i1);
+        int root2 = find(i2);
+        if (root1 == root2)
+            return;
+        
+        if (this.sizeComponent[root1] < this.sizeComponent[root2]) {
+            this.sizeComponent[root2] += this.sizeComponent[root1];
+            this.parents[root1] = root2;
+        } else {
+            this.sizeComponent[root1] += this.sizeComponent[root2];
+            this.parents[root2] = root1;
+        }
+        
+        this.numComponents--;
+    }
+}
