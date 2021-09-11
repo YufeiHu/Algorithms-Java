e639fad37e74d1da44098f7ec482b3440b3044ca
Yufei Hu <yufeihu@g.ucla.edu>
2021-09-11 16:46:50 -0700

    Update TopologicalSort.java

--- TopologicalSort.java
+++ TopologicalSort.java
@@ -1,12 +1,4 @@
-public static void reverseList(List<Integer> nums) {
-    for(int i = 0; i < nums.size() / 2; i++) {
-        int tmp = nums.get(i);
-        nums.set(i, nums.get(nums.size() - i - 1));
-        nums.set(nums.size() - i - 1, tmp);
-    }
-}
-
-public boolean topologicalSortHelper(Map<Integer, List<Integer>> graph, int[] visited, List<Integer> ans, int node) {
+private boolean topologicalSortHelper(Map<Integer, List<Integer>> graph, int[] visited, List<Integer> ans, int node) {
     // 1: visited
     // 0: unvisited
     // -1: visiting
@@ -30,13 +22,12 @@ public boolean topologicalSortHelper(Map<Integer, List<Integer>> graph, int[] vi
     }
 }
 
-public boolean topologicalSort(Map<Integer, List<Integer>> graph, List<Integer> ans, int n) {
-    int[] visited = new int[n];
-    for (int node = 0; node < n; node++) {
-        if (!topologicalSortHelper(graph, visited, ans, node)) {
+public boolean topologicalSort(Map<Integer, List<Integer>> graph, List<Integer> ans, int numNodes) {
+    int[] visited = new int[numNodes];
+    for (int node = 0; node < numNodes; node++) {
+        if (!topologicalSortHelper(graph, visited, ans, node))
             return false;
-        }
     }
-    reverseList(ans);
+    Collections.reverse(ans);
     return true;
 }
