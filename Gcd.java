c8eaa91da8a84746312e5a10d71b74d05b06b51f
Yufei Hu <yufeihu@g.ucla.edu>
2021-09-14 20:41:40 -0700

    Update Gcd.java

--- Gcd.java
+++ Gcd.java
@@ -1,3 +1,6 @@
+/*
+ * Get the greatest common divisor.
+ */
 public int getGcd(int a, int b) {
     while (b > 0) {
         int tmp = a;
@@ -5,4 +8,11 @@ public int getGcd(int a, int b) {
         b = tmp % b;
     }
     return a;
+}
+
+/*
+ * Get the least common multiple.
+ */
+public int getLcm(int a, int b) {
+    return a * b / getGcd(a, b);
 }
