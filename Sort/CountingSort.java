21693c04f2431ff6abdcda68261d62771ced7f66
Yufei Hu <yufeihu@g.ucla.edu>
2021-09-18 17:55:28 -0700

    Update CountingSort.java

--- Sort/CountingSort.java
+++ Sort/CountingSort.java
@@ -8,21 +8,26 @@
  * Apply this algorithm when O(k) <= O(n), and when all numbers in nums are >= 0.
  */
 public int[] countingSort(int[] nums) {
+    // step 1: handle corner case
     if (nums.length <= 1)
         return nums;
 
-    // get freqs
+    // step 2: get freqs
     int max = Arrays.stream(nums).max().getAsInt();
     int[] freqs = new int[max + 1];
     for (int num : nums)
         freqs[num]++;
 
-    // get accumulativeFreqs
+    // step 3: get accumulativeFreqs
     int[] accumulativeFreqs = new int[max + 1];
-    for (int i = 1; i < accumulativeFreqs.length; i++)
-        accumulativeFreqs[i] = freqs[i] + accumulativeFreqs[i - 1];
+    for (int num = 0; num <= max; num++) {
+        if (num == 0)
+            accumulativeFreqs[num] = freqs[num];
+        else
+            accumulativeFreqs[num] = freqs[num] + accumulativeFreqs[num - 1];
+    }
 
-    // get the sorted array
+    // step 4: get the sorted array
     int[] ans = new int[nums.length];
     for (int i = nums.length - 1; i >= 0; i--) {
         int num = nums[i];
