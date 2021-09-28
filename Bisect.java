580e6a50caa903b354de5b89fa33144152623526
Yufei Hu <yufeihu@g.ucla.edu>
2021-09-28 15:09:02 -0700

    Update Bisect.java

--- Bisect.java
+++ Bisect.java
@@ -1,47 +1,31 @@
 public int bisectLeft(int[] nums, int l, int r, int val) {
-    if (l > r)
+    if (l > r || l < 0 || r >= nums.length)
         return -1;
-    if (val < nums[l])
-        return l;
-    if (val > nums[r])
-        return r + 1;
-    
-    int il = l;
-    int ir = r + 1;
-    while (true) {
-        if (il + 1 == ir) {
-            if (nums[il] == val)
-                return il;
-            else
-                return il + 1;
-        }
-        
-        int im = (il + ir) / 2;
-        if (val <= nums[im])
-            ir = im;
+
+    int min = l;
+    int max = r;
+    while (min <= max) {
+        int mid = min + (max - min) / 2;
+        if (nums[mid] < val)
+            min = mid + 1;
         else
-            il = im;
+            max = mid - 1;
     }
+    return min;
 }
 
 public int bisectRight(int[] nums, int l, int r, int val) {
-    if (l > r)
+    if (l > r || l < 0 || r >= nums.length)
         return -1;
-    if (val < nums[l])
-        return l;
-    if (val > nums[r])
-        return r + 1;
 
-    int il = l;
-    int ir = r + 1;
-    while (true) {
-        if (il + 1 == ir)
-            return il + 1;
-
-        int im = (il + ir) / 2;
-        if (val < nums[im])
-            ir = im;
+    int min = l;
+    int max = r;
+    while (min <= max) {
+        int mid = min + (max - min) / 2;
+        if (nums[mid] <= val)
+            min = mid + 1;
         else
-            il = im;
+            max = mid - 1;
     }
+    return min;
 }
